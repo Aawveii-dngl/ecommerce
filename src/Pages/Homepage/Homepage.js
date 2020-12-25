9507181b09def107741a93c6cf89f390e9ff28f2
Avi Dangol <dangolavi9999@gmail.com>
2020-12-25 16:34:23 +0545

    First Commit

---
+++ src/Pages/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Directorycomp from '../../Components/Directory/Directorycomp'
+import './Homepage.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <h1>Welcome to my Homepage</h1>
+    <Directorycomp/>
+  </div>
+);
+
+export default HomePage;
