e9faa6cff50a61b10d01e0d7ad3016d3528220d1
Avi Dangol <dangolavi9999@gmail.com>
2020-12-26 21:14:40 +0545

    Adding header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header/Header';
 import Routes from './Pages/Homepage/ROUTE/Routes';
 
 function App() {
   return (
     <div >
+      <Header/>
       <Routes/>
     </div>
   );
